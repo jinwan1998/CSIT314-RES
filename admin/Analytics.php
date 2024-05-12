359cccd914a893900c4a9fc83b6a7d3f7f416878
Levaote <70951726+Levaote@users.noreply.github.com>
2024-05-12 20:24:23 +0000

    remove "admin/" from css ref

--- admin/Analytics.php
+++ admin/Analytics.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analytics - Real Estate System</title>
-    <link rel="stylesheet" type="text/css" href="admin/admin.css">
+    <link rel="stylesheet" type="text/css" href="admin.css">
     <style>
         table {
             width: 100%;
